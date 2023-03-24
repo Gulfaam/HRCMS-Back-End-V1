d4ee71dadb069689a7f50da1fb9dc7095cd7c0e0
abubakar105 <abubakar.59132@gmail.com>
2023-03-24 15:24:07 +0500

    Employee Module

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ import express from "express";
 // routes
 import userRoute from "./user/index.js";
 
+import employeeRoute from "./employee/index.js"
+
 const protectedRouter = express.Router();
 const unProtectedRouter = express.Router();
 
@@ -10,5 +12,6 @@ const unProtectedRouter = express.Router();
 
 // Un-Protected Routes
 unProtectedRouter.use("/user", userRoute);
+unProtectedRouter.use("/employee", employeeRoute);
 
 export { protectedRouter, unProtectedRouter };
--- validations/index.js
+++ validations/index.js
@@ -1,1 +1,2 @@
 export { default as authValidation } from "./user.validation"
+export { default as employeeValidation } from "./employee.validation"

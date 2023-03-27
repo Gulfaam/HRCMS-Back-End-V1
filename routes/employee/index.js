99a017cadbad19e717da47c205c29cf0a674ce3e
abubakar105 <abubakar.59132@gmail.com>
2023-03-27 14:39:30 +0500

    Employee api test

--- routes/employee/index.js
+++ routes/employee/index.js
@@ -7,8 +7,9 @@ import upload from "../../middlewares/multer.js";
 
 const router = express.Router();
 router.get("/", authenticate, controllers.getAll);
-router.post("/", authenticate, upload.single("file"), validate(employeeValidation.add), controllers.add);
+router.get("/:id", authenticate, validate(employeeValidation.id), controllers.getOne);
+router.post("/", authenticate, upload.single("image"), validate(employeeValidation.add), controllers.add);
 router.delete("/:id", authenticate, validate(employeeValidation.id), controllers.delete);
-router.patch("/:id", authenticate, validate(employeeValidation.update), controllers.update);
+router.patch("/:id", authenticate, upload.single("image"), validate(employeeValidation.update), controllers.update);
 
 export default router;

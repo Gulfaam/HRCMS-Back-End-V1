593705e35044f6bf23178d514e9f66809733ff7d
ishanazeer <ishanazeer2266@gmail.com>
2023-03-31 16:58:01 +0500

    getJobById

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import userRoute from "./user.js";
 import jobRoute from "./job.js";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 const protectedRouter = express.Router();
 const unProtectedRouter = express.Router();

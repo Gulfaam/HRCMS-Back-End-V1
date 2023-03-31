101f8b03cb4cacaf17a5db340db158dd609be6c7
ishanazeer <ishanazeer2266@gmail.com>
2023-03-31 17:17:54 +0500

    Update job by id

--- routes/job.js
+++ routes/job.js
@@ -29,8 +29,19 @@ router.get("/:id", authenticate, async (req, res) => {
   }
 });
 
-router.get("/",  async (req, res) => {
+router.get("/",authenticate, async (req, res) => {
   const addResponse = await JobService.getAll()
+  if (addResponse.message === "success") {
+    return httpResponse.CREATED(res, addResponse.data);
+  } else if (addResponse.message === "failed") {
+    return httpResponse.CONFLICT(res, addResponse.data);
+  } else {
+    return httpResponse.INTERNAL_SERVER_ERROR(res, addResponse.data);
+  }
+});
+
+router.patch("/:id",authenticate,  async (req, res) => {
+  const addResponse = await JobService.update(req.params.id, req.body);
   if (addResponse.message === "success") {
     return httpResponse.CREATED(res, addResponse.data);
   } else if (addResponse.message === "failed") {

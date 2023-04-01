2e6b313d7f3552ba29a6f38f0e48f747af80a585
sameer <sameer.ijaz100@gmail.com>
2023-04-01 10:06:53 +0500

    get all hierarchy

--- routes/Hierarchy/controllers.js
+++ routes/Hierarchy/controllers.js
@@ -14,44 +14,12 @@ const controller = {
     }
   },
 
- get: async (req, res) => {
-    try{
-       //*passing the req
-      const addResponse = await hierarchy.get(req.params.id);
-      if (addResponse.message === "success") {
-        return httpResponse.SUCCESS(res, addResponse);
-      } else {
-        return httpResponse.UNAUTHORIZED(res, addResponse.data);
-      }
-    }
-    catch (error) {
-      return httpResponse.INTERNAL_SERVER_ERROR(res, error);
-      }
-  },
-
   getAll: async (req, res) => {
     try {
       const data = await hierarchy.getAll();
       return httpResponse.SUCCESS(res, data.data);
     } catch (error) {
       return httpResponse.INTERNAL_SERVER_ERROR(res, error);
-    }
-  },
-
-  update: async (req, res) => {
-    try{
-    const addResponse = await hierarchy.update(req.params.id, req.body, {new: true} );
-    return httpResponse.SUCCESS(res, addResponse.data);
-   } catch (error) {
-    return httpResponse.NOT_FOUND(res, error);
-  }
-  },
-  delete: async (req, res) => {
-    try {
-        const addResponse = await hierarchy.delete(req.params.id);   
-        return httpResponse.SUCCESS(res, addResponse.data);  
-    } catch (error) {
-        return httpResponse.NOT_FOUND(res, error);
     }
   },
 }

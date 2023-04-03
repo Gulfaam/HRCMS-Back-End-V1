67c04f868994424d255f5c66c9e693d60802145d
sameer <sameer.ijaz100@gmail.com>
2023-04-03 22:28:24 +0500

    remove the update conflicts

--- routes/Hierarchy/controllers.js
+++ routes/Hierarchy/controllers.js
@@ -4,13 +4,17 @@ import httpResponse from "../../utils/httpResponse.js";
 
 const controller = {
   add: async (req, res) => {
+    try{ 
     const addResponse = await hierarchy.add(req.body);
     if (addResponse.message === "success") {
       return httpResponse.CREATED(res, addResponse);
     } else if (addResponse.message === "failed") {
-      return httpResponse.CONFLICT(res, addResponse.data);
-    } else {
-      return httpResponse.INTERNAL_SERVER_ERROR(res, addResponse.data);
+      return httpResponse.BAD_REQUEST(res, addResponse.data);
+    }
+   } 
+   catch(error)
+   {
+      return httpResponse.INTERNAL_SERVER_ERROR(res, error);
     }
   },
   getAll: async (req, res) => {
--- routes/Hierarchy/index.js
+++ routes/Hierarchy/index.js
@@ -7,7 +7,6 @@ import authenticate from "../../middlewares/authenticate.js";
 const router = express.Router();
 router.get("/", authenticate,controllers.getAll)
 router.post("/", authenticate,validate(authValidation.add),controllers.add);
-router.get('/:id',authValidation,validate(authValidation.id),controllers.get)
 
 
 

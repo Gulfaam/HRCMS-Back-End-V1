00cb13894b379c6261a475256325aca5aeab08c4
sameer <sameer.ijaz100@gmail.com>
2023-04-04 13:41:36 +0500

    upadting the url

--- config/env.js
+++ config/env.js
@@ -1,8 +1,8 @@
 const environment = {
   port: process.env.PORT || 2022,
   nodeEnv: process.env.NODE_ENV || "development",
-  mongodbUri: process.env.DB_URI || "mongodb://127.0.0.1:27017/HRMS",
-  email:process.env.email ||  "mongodb+srv://sameerijaz100:burak@cluster0.ddb6csg.mongodb.net/HRCMS?retryWrites=true&w=majority"
+  mongodbUri: process.env.DB_URI || "mongodb+srv://sameerijaz100:burak@cluster0.ddb6csg.mongodb.net/HRCMS?retryWrites=true&w=majority",
+  email:process.env.email ||  "mercrusolds@gmail.com"
 };
 
 export default environment;

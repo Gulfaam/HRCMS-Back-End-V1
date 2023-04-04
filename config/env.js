8815007d696255b02c6ba2fd1c44ba728db0af30
sameer <sameer.ijaz100@gmail.com>
2023-04-04 13:39:22 +0500

    email

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@ const environment = {
   port: process.env.PORT || 2022,
   nodeEnv: process.env.NODE_ENV || "development",
   mongodbUri: process.env.DB_URI || "mongodb+srv://sameerijaz100:burak@cluster0.ddb6csg.mongodb.net/HRCMS?retryWrites=true&",
-  email:process.env.email || "mercurysols",
+  email:process.env.email || "mercurysols@gmail.com",
 };
 
 export default environment;

1daed251350a1b544e549cbee74da0b73569bb83
ishanazeer <ishanazeer2266@gmail.com>
2023-04-05 23:57:39 +0500

    FEAT: all the APIs of permission module

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ import express from "express";
 import userRoute from "./user/index.js";
 import attendenceRoute from "./attendence/index.js";
 import miscRoute from "./misc/index.js";
-import eventRoute from "./event/index.js"
+import eventRoute from "./event/index.js";
 import hierarchyRoute from "./Hierarchy/index.js";
+import permissionRoute from "./permission/index.js";
 
-
-import employeeRoute from "./employee/index.js"
+import employeeRoute from "./employee/index.js";
 
 const protectedRouter = express.Router();
 const unProtectedRouter = express.Router();
@@ -22,5 +22,6 @@ unProtectedRouter.use("/attendence", attendenceRoute);
 unProtectedRouter.use("/misc", miscRoute);
 unProtectedRouter.use("/event", eventRoute);
 unProtectedRouter.use("/hierarchy", hierarchyRoute);
+unProtectedRouter.use("/permission", permissionRoute);
 
 export { protectedRouter, unProtectedRouter };

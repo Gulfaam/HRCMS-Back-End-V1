50559312df2bee750bee7906e5f73fc852d8bbdc
abubakar105 <114481859+abubakar105@users.noreply.github.com>
2023-04-10 13:54:29 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 import express from "express";
-
-// routes
 import userRoute from "./user/index.js";
 import attendenceRoute from "./attendence/index.js";
 import miscRoute from "./misc/index.js";
-
 import payrollRoute from "./payroll/index.js"
-
 import eventRoute from "./event/index.js"
-
-
-
 import employeeRoute from "./employee/index.js"
 
 const protectedRouter = express.Router();
@@ -23,9 +16,7 @@ unProtectedRouter.use("/user", userRoute);
 unProtectedRouter.use("/employee", employeeRoute);
 unProtectedRouter.use("/attendence", attendenceRoute);
 unProtectedRouter.use("/misc", miscRoute);
-
 unProtectedRouter.use("/payroll", payrollRoute);
-
 unProtectedRouter.use("/event", eventRoute);
 
 

8a95a4cb1075a119039df4083d37345de01a6874
sameer <sameer.ijaz100@gmail.com>
2023-04-19 14:26:44 +0500

    Complete

--- routes/Hierarchy/controllers.js
+++ routes/Hierarchy/controllers.js
@@ -3,40 +3,74 @@ import httpResponse from "../../utils/httpResponse.js";
 
 const controller = {
   add: async (req, res) => {
-    try {
+    try{
       const addResponse = await hierarchy.add(req.body);
-      if (addResponse.message === "success") {
-        return httpResponse.CREATED(res, addResponse);
-      } else if (addResponse.message === "failed") {
-        return httpResponse.BAD_REQUEST(res, addResponse.data);
-      } else {
-        return httpResponse.INTERNAL_SERVER_ERROR(res, addResponse.data);
+      if(!addResponse.data){
+      return httpResponse.CONFLICT(res,addResponse.data);
       }
-    } catch (error) {
-      return httpResponse.INTERNAL_SERVER_ERROR(res, error);
+      else{
+        return httpResponse.SUCCESS(res,addResponse.data)
+      }
+    }
+    catch(addResponse) {
+       return httpResponse.INTERNAL_SERVER_ERROR(res,addResponse.data)
+  }
+},
+  
+  getOneById: async (req, res) => {
+    try{
+    const addResponse = await hierarchy.getOneById(req.params.id);
+    if(!addResponse.data){
+      return httpResponse.NOT_FOUND(res,addResponse.data);
+    }
+    else {
+      return httpResponse.SUCCESS(res,addResponse.data);
+    }
+    }
+    catch (error){
+        return httpResponse.INTERNAL_SERVER_ERROR(res,error.data);
     }
   },
-
-  get: async (req, res) => {
-    try {
-      const addResponse = await hierarchy.get(req.params.id);
-      if (addResponse.message === "success") {
-        return httpResponse.CREATED(res, addResponse);
-      } else if (addResponse.message === "failed") {
-        return httpResponse.BAD_REQUEST(res, addResponse.data);
-      }
-    } catch (error) {
-      return httpResponse.INTERNAL_SERVER_ERROR(res, error);
-    }
-  },
+  
   getAll: async (req, res) => {
     try {
       const data = await hierarchy.getAll();
       return httpResponse.SUCCESS(res, data.data);
     } catch (error) {
-      return httpResponse.INTERNAL_SERVER_ERROR(res, error);
+      return httpResponse.INTERNAL_SERVER_ERROR(res,error.data);
     }
   },
-};
+
+  updateOneById:async (req,res) => {
+    try{
+      const data =await hierarchy.updateOneById(req.params.id, req.body, {new: true});
+      if(!data.data){
+      return httpResponse.NOT_FOUND(res,data.data)
+      }
+      else{
+        return httpResponse.SUCCESS(res,data.data);
+      }
+    }
+    catch(error){
+      return httpResponse.INTERNAL_SERVER_ERROR(res, error.data);
+    }
+  },
+
+  deleteOneById:async (req,res) => {
+    try{
+      const data =await hierarchy.deleteOneById(req.params.id);
+      if(!data.data){
+      httpResponse.NOT_FOUND(res,data.data);
+      }
+     else{
+      return httpResponse.SUCCESS(res,data.data)
+    }
+  }
+    catch(error){
+      return httpResponse.INTERNAL_SERVER_ERROR(res,error.data);
+    }
+  }
+}
+
 
 export default controller;
